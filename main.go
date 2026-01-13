f0bfb62893509c4bee27939a73b6b3d11596dc3a
Sourya Roy <souryaroy30@gmail.com>
2026-01-13 19:26:45 +0530

    codebase restructuring and the cli arguemnt for taking username

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Error: Username is required.")
+		fmt.Println("Usage: github-user-activity <username>")
+		os.Exit(1)
+	}
+	username := os.Args[1]
+	fmt.Printf("Fetching activity for: %s...\n", username)
+
+}
